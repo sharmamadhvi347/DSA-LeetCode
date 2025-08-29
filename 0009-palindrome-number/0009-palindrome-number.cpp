92f491d9cf3562fe6d995df5f91cf167d08fa718
Madhvi Sharma <sharmamadhvi347@gmail.com>
2025-08-30 00:19:46 +0530

    Time: 0 ms (100%), Space: 8.7 MB (40.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return false;
+        long long rev = 0;
+        int n = x;
+        while (n != 0) {
+            int digit = n % 10;
+            n /= 10;
+            rev = rev * 10 + digit;
+
+            if (rev > INT_MAX) 
+                return 0;
+        }
+        return(x==rev);
+    }
+};
