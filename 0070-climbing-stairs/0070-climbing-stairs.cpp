598b9f0749465ab150dfc3731f82b0dc8f215c8c
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-02-14 12:22:20 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int dp[46];
+    int dp[100];
 
     int climbStairs(int n) {
-        if(n <= 2) return n;
+        if(n<=2) return n;
 
         if(dp[n] != 0) return dp[n];
 
