bf3295713b9bab6eed8d89b6f86396d1ff7b5bef
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-03-22 21:52:44 +0530

    Time: 2 ms (78.93%), Space: 7.7 MB (64.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
 
-    int arr[100];
+    // int arr[100];
+    // int fib(int n) {
+    //     if(n == 0 )return 0;
+    //     if(n == 1) return 1;
+
+    //     if (arr[n] !=0 )return arr[n];
+
+    //     arr[n] = fib(n-1) + fib(n-2);
+    //     return arr[n];
+    // }
+
     int fib(int n) {
-        if(n == 0 )return 0;
-        if(n == 1) return 1;
-
-        if (arr[n] !=0 )return arr[n];
-
-        arr[n] = fib(n-1) + fib(n-2);
-        return arr[n];
+        if(n<=1) return n;
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
