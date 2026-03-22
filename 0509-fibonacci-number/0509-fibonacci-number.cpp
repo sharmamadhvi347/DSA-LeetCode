39ddab3882ecf581b7c6824c6ed8998d3e80d2a3
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-03-22 21:52:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.35%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+    int arr[100];
+    int fib(int n) {
+        if(n == 0 )return 0;
+        if(n == 1) return 1;
+
+        if (arr[n] !=0 )return arr[n];
+
+        arr[n] = fib(n-1) + fib(n-2);
+        return arr[n];
+    }
+};
