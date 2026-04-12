b089f3e1a4b27c123961a777dcbf119ad45f380d
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-04-12 22:46:39 +0530

    Time: 12 ms (43.43%), Space: 11.9 MB (52.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -21,7 +21,8 @@ public:
 
         while (ptr2 != NULL && ptr2->next != NULL) {
             ptr1 = ptr1->next;
-            ptr2 = ptr2->next->next;
+            ptr2 = ptr2->next;
+            ptr2 = ptr2->next;
 
             if (ptr1 == ptr2) return true;
         }
