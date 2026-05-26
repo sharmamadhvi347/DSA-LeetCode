36b1073fdfb4fd7503d52f0678f5ae170b03a8c3
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-26 15:31:24 +0530

    Time: 0 ms (100%), Space: 20.6 MB (57.62%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,20 @@
+#include<algorithm>
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int max=nums[n-1];//max should be set after sorting not before!!, you did this mistake here so be careful for further ques
+
+        for(int i = 0; i<n-1; i=i+2){
+            if(nums[i]==nums[i+1]) continue;
+            else{
+                max = nums[i];
+                break;
+            }
+        }
+
+        return max;
+    }
+};
