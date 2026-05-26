0742b51d5a1eb3729e90d4ebc6f76b0f85330d84
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-26 14:57:34 +0530

    Time: 0 ms (100%), Space: 29.7 MB (40.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,9 +6,17 @@ public:
         int n = nums.size();
         if(k>n) k = k%n;
 
+        //can be solved without this reverse function 
+        //using temp variable and doing normal swaping
+        //it's just that solution would be lengthier with that
+        //and both of them are done in O(1) Space Complexity
+
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin()+k, nums.end());
+
+        //another solution would be using 1 more array and just simply copying elements from 
+        //nums according to k 
         
         return;
     }
