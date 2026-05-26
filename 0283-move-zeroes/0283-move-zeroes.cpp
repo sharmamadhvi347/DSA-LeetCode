4c9169e24da7545935cb6975388a627bb1c46a21
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-26 15:07:16 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+
+        //DID THIS QUESTION IN 1 GO AND 5 MINS😁,YAYYYY
         int n = nums.size();
         int i = 0;
         int j =1;
