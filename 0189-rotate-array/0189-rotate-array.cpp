4b37423cd8b3f0193942f83d6e718e4d4220ea8a
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-26 14:55:20 +0530

    Time: 0 ms (100%), Space: 29.5 MB (65.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,13 +5,11 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         if(k>n) k = k%n;
-        // if(k<=n){
-            reverse(nums.begin(), nums.end());
-            reverse(nums.begin(), nums.begin() + k);
-            reverse(nums.begin()+k, nums.end());
-        // }
-        
 
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin()+k, nums.end());
+        
         return;
     }
 };
