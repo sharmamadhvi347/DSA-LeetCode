0367da8fa7132d18340f68f40d239228c22e1656
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-27 00:01:50 +0530

    Time: 0 ms (100%), Space: 14.9 MB (37.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,6 +3,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //TC=O(1)-> using hash maps
+        //TC = O(n^2) -> using 2 for loops for searching🤮
         unordered_map<int,int> mp;
 
         for(int i=0; i<nums.size(); i++) {
@@ -10,7 +12,7 @@ public:
             int need = target - nums[i];
 
             if(mp.find(need) != mp.end()) {
-                return {mp[need], i};
+                return {i,mp[need]};
             }
 
             mp[nums[i]] = i;
