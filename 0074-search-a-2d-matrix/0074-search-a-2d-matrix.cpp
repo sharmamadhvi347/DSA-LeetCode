bbf587c1e1f345ccb3f38702ec7ebc8654949379
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-06-20 15:12:12 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.06%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();        // rows
+        int n = matrix[0].size();     // columns
+        long long lo = 0;
+        long long high = m*n-1;
+
+        //almost everything is fine- just the syntax can be wrong of matrix and 2nd high and low altration in while loop, I am sure about rest.
+
+        while(lo<=high){
+            long long mid = (high+lo)/2;
+            long long a = mid/n;
+            long long b = mid %n;
+            long long element = matrix[a][b];
+            
+            if(element > target) high = mid-1;
+            if(element < target) lo = mid + 1;
+            if(element == target ) return true;
+        }
+
+        return false;
+    }
+};
