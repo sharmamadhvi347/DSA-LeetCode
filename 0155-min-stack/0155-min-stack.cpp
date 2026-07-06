e92f1a679a609345945f6105ba811a3621baa388
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-07-07 01:24:12 +0530

    Time: 46 ms (59.77%), Space: 151.2 MB (33.22%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,3 +1,33 @@
+// best sol so far:: loved it: like using 2 places in stack to store current and min till now, this way we dont ahve to create another stack to keep track of min
+
+//class MinStack {
+// public:
+//     stack<pair<int, int>> st;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if (!st.empty()) {
+//             st.push({val, min(val, st.top().second)});
+//         } else {
+//             st.push({val, val});
+//         }
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
 class MinStack {
 public:
     stack <int> minStack;
