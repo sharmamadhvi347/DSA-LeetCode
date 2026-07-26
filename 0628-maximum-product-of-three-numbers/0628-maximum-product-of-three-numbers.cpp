cd0fd086021fbfce2334e9793c8d5acc831ceb52
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-07-26 22:36:25 +0530

    Time: 0 ms (100%), Space: 31.4 MB (96.8%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,16 +1,52 @@
-#include <algorithm>
+// #include <algorithm>
+
+// class Solution {
+// public:
+//     int maximumProduct(vector<int>& nums) {
+//         ranges::sort(nums);
+//         reverse(nums.begin(), nums.end());
+//         int max1 = nums[0]*nums[1]*nums[2];
+//         int min = max(nums[0]*nums[nums.size()-1]*nums[nums.size()-2], nums[0]*nums[nums.size()-1]*nums[1]);
+        
+
+
+//         return max(max1, min);
+        
+//     }
+// };
+
+
+//optimal one: keeping track of only three largest and two smallest- O(n)
 
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        ranges::sort(nums);
-        reverse(nums.begin(), nums.end());
-        int max1 = nums[0]*nums[1]*nums[2];
-        int min = max(nums[0]*nums[nums.size()-1]*nums[nums.size()-2], nums[0]*nums[nums.size()-1]*nums[1]);
-        
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
 
+        for (int x : nums) {
+            // Update three largest
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3) {
+                max3 = x;
+            }
 
-        return max(max1, min);
-        
+            // Update two smallest
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2) {
+                min2 = x;
+            }
+        }
+
+        return max(max1 * max2 * max3,
+                   max1 * min1 * min2);
     }
 };
